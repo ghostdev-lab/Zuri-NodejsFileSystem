d757f426e2ae34be2d7482033129b5df25fee592
ekene enyi <42587495+Allen-Codz@users.noreply.github.com>
2021-04-15 09:59:56 +0100

    Add files via upload

---
+++ Nodejs FileSystem task/fileSystem.js
@@ -0,0 +1,26 @@
+//Called the fetch module
+var fetch = require('node-fetch');
+//Called the fs (fileSystem) module
+var fs = require("fs");
+
+//Created a variable (url) to contain the url of the api
+var url = 'http://jsonplaceholder.typicode.com/posts';
+
+//Used the fetch method to request data from the api
+fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log("Fetching json data successfully.");
+    
+        /*Created a variable (jsondata) and used the JSON.stringify function 
+          to convert the json data gotten from the api to a string*/
+        var jsondata = JSON.stringify(json, null, 2);
+        
+        //Used the writeFile function to write the json data to a new file
+        fs.writeFile("./result/posts.json", jsondata , err => {
+            
+            //If error occurs throw error on screen
+            if (err) throw err;
+            console.log("Done writing.")
+        })
+    })
